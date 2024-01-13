fbae5ae5afbce62be8381bedb6405caa24551fcf
sambhavjoshi <121726304+sambhavjoshi@users.noreply.github.com>
2024-01-14 01:11:03 +0530

    Add files via upload

---
+++ backend/models/itemModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+  name: String,
+  cost: Number,
+  stock: Number,
+  damaged:{
+    type:Number,
+    default:0,
+  },
+  lab:String,
+  log: [
+    {
+      category: String,
+      stock: Number,
+      date: {
+        type: Date,
+        default: Date.now(),
+      },
+      cost: Number,
+      approvedBy: String,
+      totalCost: Number,
+      askedBy:String,
+    },
+  ],
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+module.exports = Item;
