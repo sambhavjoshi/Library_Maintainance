8039c64ef3b60b43c431247f2d88d3e92ac785e5
sambhavjoshi <121726304+sambhavjoshi@users.noreply.github.com>
2024-01-18 14:07:55 +0530

    Update schoolStudentRoute.js

--- backend/routes/schoolStudentRoute.js
+++ backend/routes/schoolStudentRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { isAuthUser} = require("../middleware/auth");
-const { createSchoolStudent, getAllSchoolStudents, payFeeSchool, getschoolStudent, updateSchoolStudent, deleteSchoolStudent } = require("../controllers/schoolStudentController");
+const { createSchoolStudent, getAllSchoolStudents, payFeeSchool, getschoolStudent, updateSchoolStudent, deleteSchoolStudent, updateGrade } = require("../controllers/schoolStudentController");
 
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.route("/schoolStudent/create").post(isAuthUser, createSchoolStudent);
 router.route("/schoolStudents").get(isAuthUser, getAllSchoolStudents);
 router.route("/schoolStudent/payfee/:id").put(isAuthUser, payFeeSchool);  
+router.route("/schoolStudent/updateGrade/:id").put(isAuthUser,updateGrade);
 router
   .route("/schoolStudent/:id")
   .get(isAuthUser, getschoolStudent)
