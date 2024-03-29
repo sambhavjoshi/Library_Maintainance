b63963eb25a3be2d90d64773dd0b5091d6530d82
sambhavjoshi <121726304+sambhavjoshi@users.noreply.github.com>
2024-03-29 13:03:21 +0530

    Update staffRoute.js

--- backend/routes/staffRoute.js
+++ backend/routes/staffRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { isAuthUser} = require("../middleware/auth");
-const { createStaff, getAllStaff, getStaffDetails, updateStaff, deleteStaff } = require('../controllers/staffController');
+const { createStaff, getAllStaff, getStaffDetails, updateStaff, deleteStaff, refreshLeaves, getCurrentYear, createMisc } = require('../controllers/staffController');
 
 const router = express.Router();
 
 router.route("/staff/create").post(isAuthUser,createStaff);
+router.route("/staff/refresh").put(isAuthUser,refreshLeaves);
+router.route("/staff/currentYear").get(isAuthUser,getCurrentYear);
 router.route("/staffs").get(isAuthUser,getAllStaff);
 router
       .route("/staff/:id")
