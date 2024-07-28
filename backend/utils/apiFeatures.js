03d0ec5d7a929bb03943fb94a5e2712f585bcc78
sambhavjoshi <121726304+sambhavjoshi@users.noreply.github.com>
2024-07-28 23:09:12 +0530

    Update apiFeatures.js

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -9,86 +9,103 @@ class ApiFeatures {
     if(!this.queryStr) {
       
     }
-    else if(this.queryStr.name){
+    if(this.queryStr.name){
           keyword = {
+            ...keyword,
             name: {
               $regex: this.queryStr.name,
               $options: "i", // makes it case insensitive
             },
           };
     }
-    else if(this.queryStr.author){
+    if(this.queryStr.author){
             keyword = {
+              ...keyword,
               author: {
                 $regex: this.queryStr.author,
                 $options: "i", // makes it case insensitive
               },
             };
     }
-    else if(this.queryStr.number){
+    if(this.queryStr.number){
           keyword = {
+            ...keyword,
             number: this.queryStr.number,
           };
     }
-    else if(this.queryStr.phone){
+    if(this.queryStr.phone){
       keyword = {
+        ...keyword,
         phone: this.queryStr.phone,
       };
     }
-    else if(this.queryStr.category){
+    if(this.queryStr.category){
       keyword = {
+        ...keyword,
         category: {
           $regex: this.queryStr.category,
           $options: "i",
         }
       };
     }
-    else if(this.queryStr.degree){
+    if(this.queryStr.degree){
       keyword = {
+        ...keyword,
         degree: this.queryStr.degree.toUpperCase(),
       };
     }
-    else if(this.queryStr.qualification){
+    if(this.queryStr.qualification){
       keyword = {
+        ...keyword,
         qualification: this.queryStr.qualification.toLowerCase(),
       };
     }
-    else if(this.queryStr.designation){
+    if(this.queryStr.designation){
       keyword = {
+        ...keyword,
         designation: this.queryStr.designation.toUpperCase(),
       };
     }
-    else if(this.queryStr.subject){
+    if(this.queryStr.subject){
       keyword = {
+        ...keyword,
         subject: this.queryStr.subject.toUpperCase(),
       };
     }
-    else if(this.queryStr.stream){
+    if(this.queryStr.stream){
       keyword = {
+        ...keyword,
         stream: {
           $regex: this.queryStr.stream,
           $options: "i",
         }
       };
     }
-    else if(this.queryStr.semester){
+    if(this.queryStr.semester){
       keyword = {
+        ...keyword,
         semester: this.queryStr.semester,
       };
     }
-    else if(this.queryStr.rollNo){
+    if(this.queryStr.rollNo){
       keyword = {
-        rollNo: this.queryStr.rollNo,
+        ...keyword,
+        rollNo: {
+          $regex: this.queryStr.rollNo,
+          $options: "i",
+        }
       };
     }
-    else if(this.queryStr.hostel){
+    if(this.queryStr.hostel){
       this.queryStr.hostel = this.queryStr.hostel.toUpperCase();
       keyword = {
+        ...keyword,
         hostel: this.queryStr.hostel === "YES" ? true : false,
       };
     }
-    else if(this.queryStr.grade){
+    if(this.queryStr.grade){
       keyword = {
+        ...keyword,
         grade: this.queryStr.grade,
       };
     }
